87cba2a163736423e60bde5df19c7127bc257179
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-15 01:21:44 +0600

    Commenting fetching)

--- src/components/JobsContainer/JobsContainer.jsx
+++ src/components/JobsContainer/JobsContainer.jsx
@@ -4,14 +4,15 @@ import Section from '../Section/Section';
 
 const JobsContainer = async ({ limit }) => {
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/jobs?limit=${limit || 0}`)
-    const data = await res.json()
-    const jobs = await data?.data
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/jobs?limit=${limit || 0}`)
+    // const data = await res.json()
+    // const jobs = await data?.data
+    const jobs = []
 
     return (
         <Section>
             <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
-                {jobs.map((job) => (
+                {jobs?.map((job) => (
                     <JobCard key={job._id} job={job} />
                 ))}
             </div>
