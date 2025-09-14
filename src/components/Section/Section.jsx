63fa9e7f35c998894809d418e28cca64c01175bb
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-15 01:12:32 +0600

    refactor: Jobscontainer added to trending jobs

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Section = ({ children }) => {
     return (
-        <section className="py-16 md:py-24 bg-bg-container ">
+        <section className="py-16 md:py-24 ">
             <div className='container mx-auto px-6'>
                 {children}
             </div>
--- src/sections/TrendingJobs.jsx
+++ src/sections/TrendingJobs.jsx
@@ -1,39 +1,6 @@
 import React from "react";
 import { Button } from "antd";
-import JobCard from "@/components/JobCard/JobCard";
-
-const jobs = [
-    {
-        id: 1,
-        position: "Frontend Developer",
-        company: "Microsoft",
-        logo: "https://pbs.twimg.com/profile_images/1919293580646010880/ZSHWjszR_400x400.png",
-        description: "Work on modern web applications using React and Next.js.",
-        location: "New York, USA",
-        salary: 4500,
-        monthly: true,
-    },
-    {
-        id: 2,
-        position: "UI/UX Designer",
-        company: "Apple",
-        logo: "https://yt3.googleusercontent.com/s5hlNKKDDQWjFGzYNnh8UeOW2j2w6id-cZGx7GdAA3d5Fu7zEi7ZMXEyslysuQUKigXNxtAB=s900-c-k-c0x00ffffff-no-rj",
-        description: "Create intuitive and beautiful user interfaces.",
-        location: "San Francisco, USA",
-        salary: 4000,
-        monthly: true,
-    },
-    {
-        id: 3,
-        position: "Backend Developer",
-        company: "Tesla",
-        logo: "https://static.vecteezy.com/system/resources/previews/020/336/484/non_2x/tesla-logo-tesla-icon-transparent-png-free-vector.jpg",
-        description: "Build scalable APIs and microservices with Node.js.",
-        location: "Remote",
-        salary: 5000,
-        monthly: true,
-    },
-];
+import JobsContainer from "@/components/JobsContainer/JobsContainer";
 
 
 export default function TrendingJobs() {
@@ -49,11 +16,7 @@ export default function TrendingJobs() {
                 </div>
 
                 {/* Job Cards */}
-                <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
-                    {jobs.map((job) => (
-                        <JobCard key={job.id} job={job} />
-                    ))}
-                </div>
+                <JobsContainer limit={3}/>
             </div>
         </section>
     );
