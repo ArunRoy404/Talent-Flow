c6337a16b3c15f64da02b99ff4c370b09359ed33
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-15 14:53:46 +0600

    feat: job apply implemented

---
+++ src/app/api/applications/route.js
@@ -0,0 +1,23 @@
+import dbConnect from "@/lib/dbConnect";
+import { NextResponse } from "next/server";
+
+// export const GET = async (req, res) => {
+//     const { searchParams } = new URL(req.url);
+//     const limit = parseFloat(searchParams.get("limit")) || 0
+
+//     // const addedBy = searchParams.get("addedBy");
+//     // const query = addedBy ? { addedBy } : {};
+
+//     const jobsCollection = dbConnect('jobs')
+//     // const data = await jobsCollection.find(query).limit(limit).toArray()
+//     const data = await jobsCollection.find().limit(limit).toArray()
+//     return NextResponse.json({ data })
+// }
+
+
+export const POST = async (req, res) => {
+    const postedData = await req.json()
+    const applicationsCollection = dbConnect('applications')
+    const data = await applicationsCollection.insertOne(postedData)
+    return NextResponse.json({ data })
+}
--- src/components/JobDetails/ApplyButton.jsx
+++ src/components/JobDetails/ApplyButton.jsx
@@ -1,13 +1,51 @@
+'use client'
+
 import { Button } from 'antd';
-import React from 'react';
+import { useSession } from 'next-auth/react';
+import React, { useState } from 'react';
+import Loader from '../Loader/Loader';
+import { toast } from 'sonner';
+import axios from 'axios';
+
+const postApplication = async (data) => {
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/applications`, data)
+    return res
+}
 
 const ApplyButton = ({ job }) => {
+    const [isLoading, setIsLoading] = useState(false)
+    const { data: session } = useSession();
+
+    const handleApply = async () => {
+        setIsLoading(true)
+        const applicationData = {
+            applicantName: session?.user?.name,
+            applicantEmail: session?.user?.email,
+            jobID: job._id,
+            status: 'pending'
+        }
+
+        try{
+            await postApplication(applicationData)
+            toast.success('Successfully Applied')
+        }catch{
+            toast.error("Application failed")
+        }finally{
+            setIsLoading(false)
+        }
+    }
     return (
         <Button
+            disabled={isLoading}
+            onClick={handleApply}
             type="primary"
             className="!bg-primary !text-secondary !px-6 !py-2 !rounded-lg"
         >
-            Apply Now
+            {
+                isLoading
+                    ? <Loader size="20" stroke="3" />
+                    : 'Apply Now'
+            }
         </Button>
     );
 };
