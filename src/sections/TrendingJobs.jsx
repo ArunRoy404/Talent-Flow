807593d686f84410478a46703a7b919ea4f2a3f1
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-15 21:57:31 +0600

    Title and description changed

--- src/sections/TrendingJobs.jsx
+++ src/sections/TrendingJobs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "antd";
 import JobsContainer from "@/components/JobsContainer/JobsContainer";
+import Link from "next/link";
 
 
 export default function TrendingJobs() {
@@ -10,13 +11,15 @@ export default function TrendingJobs() {
                 {/* Header */}
                 <div className="flex justify-between items-center mb-8">
                     <h2 className="text-3xl font-bold text-secondary">Trending Jobs</h2>
-                    <Button type="text">
-                        See All Jobs
-                    </Button>
+                    <Link href='/jobs'>
+                        <Button type="text">
+                            See All Jobs
+                        </Button>
+                    </Link>
                 </div>
 
                 {/* Job Cards */}
-                <JobsContainer limit={3}/>
+                <JobsContainer limit={3} />
             </div>
         </section>
     );
