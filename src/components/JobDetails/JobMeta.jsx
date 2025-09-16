39ddae1e2c05fdf8136f751dec4a7a002f084238
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-17 00:41:06 +0600

    fix: Date fixed

--- src/components/JobDetails/JobMeta.jsx
+++ src/components/JobDetails/JobMeta.jsx
@@ -7,7 +7,7 @@ const JobMeta = ({job}) => {
             <Tag color="blue">{job.jobType}</Tag>
             <Tag color="green">${job.salary}/month</Tag>
             <Tag color="orange">{job.location}</Tag>
-            <Tag color="default">Posted {new Date(job.postedAt).toDateString()}</Tag>
+            <Tag color="default">Posted {new Date(job.dateAdded).toDateString()}</Tag>
         </div>
     );
 };
