d7ebbddd3e54ea3c6bf540567e82371912ca9d42
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-17 15:16:55 +0600

    refactio: SigninPage created

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,13 +1,5 @@
-import GoogleSignIn from "@/components/UI/GoogleSignIn";
+import SigninPage from '@/pages/Auth/SigninPage';
 
-export default function SignIn() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-white px-4">
-            <div className="max-w-md w-full text-center space-y-6">
-                <h1 className="text-3xl font-bold text-secondary">Sign in to TalentFlow</h1>
-                <p className="text-secondary text-sm">Use your Google account to continue</p>
-                <GoogleSignIn/>
-            </div>
-        </div>
-    );
-}
+const page = () => <SigninPage/>
+
+export default page;
---
+++ src/pages/Auth/SigninPage.jsx
@@ -0,0 +1,15 @@
+import GoogleSignIn from "@/components/UI/GoogleSignIn";
+
+const SigninPage = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-white px-4">
+            <div className="max-w-md w-full text-center space-y-6">
+                <h1 className="text-3xl font-bold text-secondary">Sign in to TalentFlow</h1>
+                <p className="text-secondary text-sm">Use your Google account to continue</p>
+                <GoogleSignIn />
+            </div>
+        </div>
+    );
+};
+
+export default SigninPage;
