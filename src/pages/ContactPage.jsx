d71775c9e5927cf4807c041444af434b78f41521
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-09-17 15:22:47 +0600

    refactio: ContactPage created

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,36 @@
+import ContactForm from "@/components/Contact/ContactForm";
+import ContactInfo from "@/components/Contact/ContactInfo";
+import PageBanner from "@/components/PageBanner/PageBanner";
+import PageDescription from "@/components/PageBanner/PageDescription";
+import PageTitle from "@/components/PageBanner/PageTitle";
+import { Fade } from "react-awesome-reveal";
+
+const ContactPage = () => {
+    return (
+        <main className="bg-base text-text-base">
+            <Fade triggerOnce>
+                {/* Hero Section */}
+                <PageBanner>
+                    <PageTitle>
+                        Get in Touch
+                    </PageTitle>
+                    <PageDescription>
+                        Have questions or need support? Fill out the form below and our team
+                        will get back to you shortly.
+                    </PageDescription>
+                </PageBanner>
+
+                {/* Contact Form & Info */}
+                <section className="py-16 md:py-24 px-6 container mx-auto grid md:grid-cols-2 gap-12">
+                    {/* Contact Form */}
+                    <ContactForm />
+
+                    {/* Contact Info */}
+                    <ContactInfo />
+                </section>
+            </Fade>
+        </main>
+    );
+};
+
+export default ContactPage;
